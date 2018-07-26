305ee3f5bcce6a2e560486cb15f88cf5c9e2d6bb
xenmayer <xenmayer@gmail.com>
2018-07-26 23:25:22 +0300

    Initial commit

---
+++ daily_generator.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python
+import os
+from datetime import datetime
+from datetime import timedelta
+from random import randint
+
+count = 1
+today = datetime.now()
+gitFake = randint(1, 3)
+
+while gitFake > 0:
+    today = today - timedelta(0, count * randint(1, 3), count * randint(2, 9), count * randint(3, 6))
+    strtoday = today.strftime('%a %b %d %H:%M:%S %Y -0600')
+    command = 'git commit --allow-empty -m "' + strtoday + '" --date="' + strtoday  + '"'
+    print command
+    os.system(command)
+    gitFake -= 1
